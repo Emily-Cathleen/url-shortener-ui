7827eaf15d0275fc93e4d50c55b4c7cad792716b
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-26 12:07:39 -0400

    Create post function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,17 @@ export const getUrls = () => {
       .then(response => response.json())
       .then((data) => data.urls)
 }
+
+export const postUrls = ({ long_url, title }) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      long_url: long_url,
+      title: title,
+    }),
+  })
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+  }
